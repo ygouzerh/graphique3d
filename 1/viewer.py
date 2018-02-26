ff8340fb72b98f31007a1f256b854cbf6cf2d46e
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.org>
2018-02-26 12:44:04 +0100

    Uniform achitecture finish. Need to pass by array instead of index

--- 1/viewer.py
+++ 1/viewer.py
@@ -9,6 +9,7 @@ import os                           # os function, i.e. checking file status
 import OpenGL.GL as GL              # standard Python OpenGL wrapper
 import glfw                         # lean window system wrapper for OpenGL
 import numpy as np                  # all matrix manipulations & OpenGL args
+from test.hello_world import helloo
 
 # Internal modules
 from transform import translate, rotate, scale, vec, frustum, perspective
@@ -70,13 +71,22 @@ void main() {
     colors_out = colors_in;
 }"""
 
-COLOR_FRAG = """#version 330 core
+COLOR_FRAG_MULTIPLE = """#version 330 core
 uniform vec3 color;
 out vec4 outColor;
 in vec3 position_out;
 in vec3 colors_out;
 void main() {
     outColor = vec4(colors_out, 1);
+}"""
+
+COLOR_FRAG_UNIFORM = """#version 330 core
+uniform vec3 color;
+out vec4 outColor;
+in vec3 position_out;
+in vec3 colors_out;
+void main() {
+    outColor = vec4(color, 1);
 }"""
 
 
@@ -130,6 +140,55 @@ class PyramideMultiColors:
         GL.glDeleteVertexArrays(1, [self.glid])
         GL.glDeleteBuffers(1, self.buffers)
 
+class PyramideUniform:
+    """Pyramid with uniform colors"""
+
+    def __init__(self):
+        # one time initialization
+        self.position = np.array(((0, 1, 0), (-0.5, 0, 0.5), (0.5, 0, 0.5), (0.5, 0, -0.5), (-0.5, 0, -0.5)), np.float32)
+        self.index = np.array((0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 1), np.uint32)
+        color = np.array(((1, 0, 0), (0, 0, 1), (0, 1, 0),(1, 1, 0), (0, 1, 1)), 'f')
+
+        self.glid = GL.glGenVertexArrays(1)            # create a vertex array OpenGL identifier
+        GL.glBindVertexArray(self.glid)                # make it active for receiving state below
+
+        self.buffers = [GL.glGenBuffers(1)]            # create one OpenGL buffer for our position attribute
+        GL.glEnableVertexAttribArray(0)           # assign state below to shader attribute layout = 0
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.buffers[0])                    # our created position buffer
+        GL.glBufferData(GL.GL_ARRAY_BUFFER, self.position, GL.GL_STATIC_DRAW)   # upload our vertex data to it
+        GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, False, 0, None)        # describe array unit as 2 floats
+
+        self.buffers += [GL.glGenBuffers(1)]                                           # create GPU index buffer
+        GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, self.buffers[-1])                  # make it active to receive
+        GL.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, self.index, GL.GL_STATIC_DRAW)     # our index array here
+
+        self.buffers += [GL.glGenBuffers(1)]                                           # create GPU index buffer
+        GL.glEnableVertexAttribArray(1)
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.buffers[-1])
+        GL.glBufferData(GL.GL_ARRAY_BUFFER, color, GL.GL_STATIC_DRAW)
+        GL.glVertexAttribPointer(1, 3, GL.GL_FLOAT, False, 0, None)        # describe array unit as 2 floats
+
+        # when drawing in the rendering loop: use glDrawArray for vertex arrays
+        # cleanup and unbind so no accidental subsequent state update
+        GL.glBindVertexArray(0)
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+
+    def draw(self, projection, view, model, color_shader, color, scaler, rotater):
+        GL.glUseProgram(color_shader.glid)
+        my_color_location = GL.glGetUniformLocation(color_shader.glid, 'color')
+        # hard coded color : (0.6, 0.6, 0.9)
+        GL.glUniform3fv(my_color_location, 1, (0.6, 0.6, 0.9))
+
+        matrix_location = GL.glGetUniformLocation(color_shader.glid, 'matrix')
+        GL.glUniformMatrix4fv(matrix_location, 1, True,
+                                perspective(35, 640/480, 0.001, 100)@translate(0.2,0,-1)@rotate(vec(0, 1, 0), -rotater)@scale(scaler))
+        GL.glBindVertexArray(self.glid)                                         # activate our vertex array
+        GL.glDrawElements(GL.GL_TRIANGLES, self.index.size, GL.GL_UNSIGNED_INT, None)  # 9 indexed verts = 3 triangles
+
+    def __del__(self):
+        GL.glDeleteVertexArrays(1, [self.glid])
+        GL.glDeleteBuffers(1, self.buffers)
+
 
 # ------------  Viewer class & window management ------------------------------
 class Viewer:
@@ -164,7 +223,8 @@ class Viewer:
         GL.glDepthFunc(GL.GL_LESS)
 
         # compile and initialize shader programs once globally
-        self.color_shader = Shader(COLOR_VERT, COLOR_FRAG)
+        self.multiple_color_shader = Shader(COLOR_VERT, COLOR_FRAG_MULTIPLE)
+        self.uniform_color_shader = Shader(COLOR_VERT, COLOR_FRAG_UNIFORM)
 
         # initially empty list of object to draw
         self.drawables = []
@@ -182,8 +242,12 @@ class Viewer:
 
             # draw our scene objects
             for drawable in self.drawables:
-                drawable.draw(None, None, None, self.color_shader, self.color,
-                            self.scaler, self.rotater)
+                if(type(drawable) is PyramideMultiColors):
+                    drawable.draw(None, None, None, self.multiple_color_shader, self.color,
+                                self.scaler, self.rotater)
+                else :
+                    drawable.draw(None, None, None, self.uniform_color_shader, self.color,
+                    self.scaler, self.rotater)
 
             # flush render commands, and swap draw buffers
             glfw.swap_buffers(self.win)
@@ -218,12 +282,14 @@ def main():
 
     # place instances of our basic objects
     viewer.add(PyramideMultiColors())
+    viewer.add(PyramideUniform())
 
     # start rendering loop
     viewer.run()
 
 
 if __name__ == '__main__':
+    helloo()
     glfw.init()                # initialize window system glfw
     main()                     # main function keeps variables locally scoped
     glfw.terminate()           # destroy all glfw windows and GL contexts
