dd5b5dc0f2a171c59e7447b0739793327a790d73
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.org>
2018-03-19 12:36:09 +0100

    Add arm hierarchy

--- 2/main.py
+++ 2/main.py
@@ -12,7 +12,7 @@ from opengl_tools.viewer import Viewer
 from opengl_tools.shader import Shader
 from opengl_tools.shaders_glsl import COLOR_VERT, COLOR_FRAG_MULTIPLE, COLOR_FRAG_UNIFORM
 from opengl_tools.loader import load
-from opengl_tools.transform import identity, translate, rotate, scale
+from opengl_tools.transform import identity, translate, rotate, scale, vec
 
 class Node:
     """ Scene graph transform and parameter broadcast node """
@@ -28,9 +28,8 @@ class Node:
         """ Recursive draw, passing down named parameters & model matrix. """
         # merge named parameters given at initialization with those given here
         param = dict(param, **self.param)
-        model = self.transform @ model
+        model = model @ self.transform
         for child in self.children:
-            print("{} appel {}".format(self.name, child))
             child.draw(projection, view, model, color_shader, **param)
 
 class Cylinder(Node):
@@ -46,9 +45,23 @@ class ViewerRoboticArm(Viewer):
         self.multiple_color_shader = Shader(COLOR_VERT, COLOR_FRAG_MULTIPLE)
 
     def do_for_each_drawable(self, drawable, view, projection, model, **param):
-        print("ROOT")
         drawable.draw(projection, view, model, self.multiple_color_shader, **param)
 
+class RotationControlNode(Node):
+    def __init__(self, key_up, key_down, axis, angle=0, **param):
+        super().__init__(**param)   # forward base constructor named arguments
+        self.angle, self.axis = angle, axis
+        self.key_up, self.key_down = key_up, key_down
+
+    def draw(self, projection, view, model, color_shader, win=None, **param):
+        assert win is not None
+        self.angle += 2 * int(glfw.get_key(win, self.key_up) == glfw.PRESS)
+        self.angle -= 2 * int(glfw.get_key(win, self.key_down) == glfw.PRESS)
+        self.transform = rotate(axis=self.axis, angle=self.angle)
+
+        # TODO : modify win in othe signatures
+        # call Node's draw method to pursue the hierarchical tree calling
+        super().draw(projection, view, model, color_shader, win=win, **param)
 # -------------- main program and scene setup --------------------------------
 def main():
     """ create a window, add scene objects, then run rendering loop """
@@ -56,9 +69,18 @@ def main():
     viewer = ViewerRoboticArm()
         # construct our robot arm hierarchy for drawing in viewer
     cylinder = Cylinder()             # re-use same cylinder instance
-    limb_shape = Node(transform=scale(x=0.1, z=0.2))  # make a thin cylinder
+    base_node_y = 0.2
+    limb_shape = Node(transform=translate(0, base_node_y, 0)@scale(0.05,1,0.05))  # make a thin cylinder
     limb_shape.add(cylinder)
-    viewer.add(limb_shape)
+
+    arm_node = RotationControlNode(glfw.KEY_LEFT, glfw.KEY_RIGHT, vec(0, 1, 0))
+    arm_node.add(limb_shape)
+
+    base_shape = Node(transform=scale(1.2,base_node_y,1.2), children=[cylinder])
+    base_node = Node(transform=rotate(axis=(0, 1, 0), angle=90)@scale(0.5,0.5,0.5), children=[base_shape])
+
+    base_node.add(arm_node)
+    viewer.add(base_node)
     # limb_shape.add(cylinder)          # common shape of arm and forearm
     #
     # arm_node = Node(transform=...)    # robot arm rotation with phi angle
