14043c5088c06cc6585d17f28cba83df5a223c8b
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.org>
2018-04-09 12:32:59 +0200

    Add the tp5
    Add the tp5 source code

---
+++ 4/main.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+"""
+Python OpenGL - Hierarchical modeling
+"""
+# Python built-in modules
+import os                           # os function, i.e. checking file status
+
+# External, non built-in modules
+import glfw                         # lean window system wrapper for OpenGL
+import numpy as np
+import OpenGL.GL as GL              # standard Python OpenGL wrapper
+from PIL import Image               # load images for textures
+from itertools import cycle
+from opengl_tools.viewer import Viewer
+from opengl_tools.shader import Shader
+from opengl_tools.loader import load
+from opengl_tools.transform import identity, translate, rotate, scale, vec
+from opengl_tools.color_mesh import ColorMesh
+from opengl_tools.node import Node, RotationControlNode
+from opengl_tools.vertex_array import VertexArray
+
+class Cylinder(Node):
+    """ Very simple cylinder based on practical 2 load function """
+    def __init__(self):
+        super().__init__()
+        self.add(*load('cylinder.obj'))  # just load the cylinder from file
+
+class Suzanne(Node):
+    """ Suzanne node """
+    def __init__(self, light_vector):
+        super().__init__()
+        # Get the only ione suzanne color mesh
+        objects = load("suzanne.obj")
+        assert len(objects)
+        self.color_mesh = objects[0]
+        # Add the light
+        self.color_mesh.addUniform3fv({"light" :light_vector})
+        print(self.color_mesh)
+        self.add(self.color_mesh)
+
+class ViewerTexture(Viewer):
+    """ Viewer for the robotic arm project """
+
+    def do_for_each_drawable(self, drawable, view, projection, model, **param):
+        drawable.draw(projection, view, model, win=self.win, **param)
+
+
+# -------------- OpenGL Texture Wrapper ---------------------------------------
+class Texture:
+    """ Helper class to create and automatically destroy textures """
+    def __init__(self, file, wrap_mode=GL.GL_REPEAT, min_filter=GL.GL_LINEAR,
+                 mag_filter=GL.GL_LINEAR_MIPMAP_LINEAR):
+        self.glid = GL.glGenTextures(1)
+        GL.glBindTexture(GL.GL_TEXTURE_2D, self.glid)
+        # helper array stores texture format for every pixel size 1..4
+        format = [GL.GL_LUMINANCE, GL.GL_LUMINANCE_ALPHA, GL.GL_RGB, GL.GL_RGBA]
+        try:
+            # imports image as a numpy array in exactly right format
+            tex = np.array(Image.open(file))
+            format = format[0 if len(tex.shape) == 2 else tex.shape[2] - 1]
+            GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, GL.GL_RGBA, tex.shape[1],
+                            tex.shape[0], 0, format, GL.GL_UNSIGNED_BYTE, tex)
+
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, wrap_mode)
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_T, wrap_mode)
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, min_filter)
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, mag_filter)
+            GL.glGenerateMipmap(GL.GL_TEXTURE_2D)
+            message = 'Loaded texture %s\t(%s, %s, %s, %s)'
+            print(message % (file, tex.shape, wrap_mode, min_filter, mag_filter))
+        except FileNotFoundError:
+            print("ERROR: unable to load texture file %s" % file)
+        GL.glBindTexture(GL.GL_TEXTURE_2D, 0)
+
+    def __del__(self):  # delete GL texture from GPU when object dies
+        GL.glDeleteTextures(self.glid)
+
+# -------------- Example texture plane class ----------------------------------
+TEXTURE_VERT = """#version 330 core
+uniform mat4 modelviewprojection;
+layout(location = 0) in vec3 position;
+out vec2 fragTexCoord;
+void main() {
+    gl_Position = modelviewprojection * vec4(position, 1);
+    fragTexCoord = position.xy;
+}"""
+
+TEXTURE_FRAG = """#version 330 core
+uniform sampler2D diffuseMap;
+in vec2 fragTexCoord;
+out vec4 outColor;
+void main() {
+    outColor = texture(diffuseMap, fragTexCoord);
+}"""
+
+class TexturedPlane:
+    """ Simple first textured object """
+
+    def __init__(self, file):
+        # feel free to move this up in the viewer as per other practicals
+        self.shader = Shader(TEXTURE_VERT, TEXTURE_FRAG)
+
+        # triangle and face buffers
+        vertices = 100 * np.array(((-1, -1, 0), (1, -1, 0), (1, 1, 0), (-1, 1, 0)), np.float32)
+        faces = np.array(((0, 1, 2), (0, 2, 3)), np.uint32)
+        self.vertex_array = VertexArray([vertices], faces)
+
+        # interactive toggles
+        self.wrap = cycle([GL.GL_REPEAT, GL.GL_MIRRORED_REPEAT,
+                           GL.GL_CLAMP_TO_BORDER, GL.GL_CLAMP_TO_EDGE])
+        self.filter = cycle([(GL.GL_NEAREST, GL.GL_NEAREST),
+                             (GL.GL_LINEAR, GL.GL_LINEAR),
+                             (GL.GL_LINEAR, GL.GL_LINEAR_MIPMAP_LINEAR)])
+        self.wrap_mode, self.filter_mode = next(self.wrap), next(self.filter)
+        self.file = file
+
+        # setup texture and upload it to GPU
+        self.texture = Texture(file, self.wrap_mode, *self.filter_mode)
+
+    def draw(self, projection, view, model, win=None, **_kwargs):
+
+        # some interactive elements
+        if glfw.get_key(win, glfw.KEY_F6) == glfw.PRESS:
+            self.wrap_mode = next(self.wrap)
+            self.texture = Texture(self.file, self.wrap_mode, *self.filter_mode)
+
+        if glfw.get_key(win, glfw.KEY_F7) == glfw.PRESS:
+            self.filter_mode = next(self.filter)
+            self.texture = Texture(self.file, self.wrap_mode, *self.filter_mode)
+
+        GL.glUseProgram(self.shader.glid)
+
+        # projection geometry
+        loc = GL.glGetUniformLocation(self.shader.glid, 'modelviewprojection')
+        GL.glUniformMatrix4fv(loc, 1, True, projection @ view @ model)
+
+        # texture access setups
+        loc = GL.glGetUniformLocation(self.shader.glid, 'diffuseMap')
+        GL.glActiveTexture(GL.GL_TEXTURE0)
+        GL.glBindTexture(GL.GL_TEXTURE_2D, self.texture.glid)
+        GL.glUniform1i(loc, 0)
+        self.vertex_array.draw(GL.GL_TRIANGLES)
+
+        # leave clean state for easier debugging
+        GL.glBindTexture(GL.GL_TEXTURE_2D, 0)
+        GL.glUseProgram(0)
+
+# -------------- main program and scene setup --------------------------------
+def main():
+    """ create a window, add scene objects, then run rendering loop """
+    viewer = ViewerTexture(TEXTURE_VERT, TEXTURE_FRAG)
+    # rotator_node = RotationControlNode(glfw.KEY_LEFT, glfw.KEY_RIGHT, vec(0, 1, 0))
+    # rotator_node.add(Suzanne(light_vector=(1, 1, 1)))
+    viewer.add(TexturedPlane("grass.png"))
+    viewer.run()
+
+if __name__ == '__main__':
+    glfw.init()                # initialize window system glfw
+    main()                     # main function keeps variables locally scoped
+    glfw.terminate()           # destroy all glfw windows and GL contexts
